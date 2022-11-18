fec6ce9c70c937a6de46711a5369a92a7678c563
agusgar409 <agusgarcia409@gmail.com>
2022-11-18 16:38:28 -0300

    add routing on nav

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,17 +16,17 @@ export class HeroesComponent implements OnInit {
   
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
+    // private messageService: MessageService
   ) { }
     
   ngOnInit(): void { this.getHeroes(); }
 
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  // }
 
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
